60f1129eb3bb737b4070d0b91151addee62fd845
mattjennings <me@mattjennings.io>
2022-03-29 19:07:40 -0500

    fix: inconsistent hmr updates on example virtual files

--- src/lib/plugin.js
+++ src/lib/plugin.js
@@ -4,13 +4,6 @@ import path from 'upath'
 import { unescape } from './util.js'
 import ast from 'abstract-syntax-tree'
 
-/**
- * Extracted examples as individual virtual files
- *
- * @type {Map<string, string>}
- */
-const virtualFiles = new Map()
-
 export default createUnplugin(
 	(
 		/**
@@ -19,6 +12,43 @@ export default createUnplugin(
 		options = {}
 	) => {
 		const { extensions = ['.svelte.md', '.md', '.svx'] } = options
+
+		/**
+		 * Extracted examples as individual virtual files
+		 *
+		 * @type {Map<string, { src: string, updated: number}>}
+		 */
+		const virtualFiles = new Map()
+
+		/**
+		 * @type {import('vite').ViteDevServer}
+		 */
+		let viteServer
+
+		/**
+		 * Iterates over each __mdsvexample_src node in generated svelte file
+		 *
+		 * @param {any} tree
+		 * @param {(srcNode: any, valueNode: any, index: number) => void} cb
+		 */
+		function iterateMdsvexSrcNodes(tree, cb) {
+			const exampleSrcNodes = ast.find(tree, {
+				type: 'Property',
+				key: {
+					name: '__mdsvexample_src'
+				}
+			})
+
+			exampleSrcNodes.map((node, i) => {
+				const [valueNode] = ast.find(node, {
+					type: 'TemplateElement'
+				})
+
+				if (valueNode) {
+					cb(node, valueNode, i)
+				}
+			})
+		}
 
 		return {
 			name: 'mdsvexamples-plugin',
@@ -33,20 +63,41 @@ export default createUnplugin(
 			load(id) {
 				if (id.includes(EXAMPLE_MODULE_PREFIX)) {
 					if (virtualFiles.has(id)) {
-						const code = virtualFiles.get(id)
+						const code = virtualFiles.get(id).src
 						return code
 					} else {
 						this.warn(`Example src not found for ${id}`)
 					}
 				}
 			},
+
 			transform(code, id) {
 				if (extensions.some((ext) => id.endsWith(ext))) {
-					updateExample(id, code)
-
 					const tree = ast.parse(code)
+					const now = Date.now()
 
 					iterateMdsvexSrcNodes(tree, (srcNode, valueNode, i) => {
+						const exampleId = `${EXAMPLE_MODULE_PREFIX}${i}.svelte`
+						const virtualId = path.join('/', path.relative(process.cwd(), id + exampleId))
+
+						// update virtualFiles with code from example
+						const prev = virtualFiles.get(virtualId) && virtualFiles.get(virtualId).src
+						const next = unescape(valueNode.value.raw)
+
+						if (next !== prev) {
+							virtualFiles.set(virtualId, { src: next, updated: now })
+
+							// invalidate module for hmr
+							if (viteServer) {
+								const mod = viteServer.moduleGraph.getModuleById(virtualId)
+								const parentMod = viteServer.moduleGraph.getModuleById(id)
+								if (mod) {
+									viteServer.moduleGraph.invalidateModule(mod)
+									viteServer.moduleGraph.invalidateModule(parentMod)
+								}
+							}
+						}
+
 						// rename the __mdsvexample_src prop to src
 						ast.replace(tree, (node) => {
 							if (node === srcNode) {
@@ -86,71 +137,35 @@ export default createUnplugin(
 			},
 
 			vite: {
+				configureServer(server) {
+					viteServer = server
+				},
 				async handleHotUpdate(ctx) {
 					const { server } = ctx
 					const modules = []
 
-					const files = [...virtualFiles.entries()]
-
-					// update the examples that are included in the updated file
+					// return virtual file modules for parent file
 					if (extensions.some((ext) => ctx.file.endsWith(ext))) {
+						const files = [...virtualFiles.entries()]
+
 						files
-							.map(([id]) => ({
+							.map(([id, file]) => ({
 								id,
-								parent: id.split(EXAMPLE_MODULE_PREFIX)[0]
+								parent: id.split(EXAMPLE_MODULE_PREFIX)[0],
+								updated: file.updated
 							}))
-							.filter((file) => ctx.file.endsWith(file.parent))
+							.filter((file) => {
+								return ctx.file.endsWith(file.parent)
+							})
 							.forEach((file) => {
-								modules.push(server.moduleGraph.getModuleById(file.id))
-								virtualFiles.set(file.id, null)
+								const mod = server.moduleGraph.getModuleById(file.id)
+								modules.push(mod)
 							})
 					}
-					return [...modules, ...ctx.modules]
+
+					return [...ctx.modules, ...modules]
 				}
 			}
 		}
 	}
 )
-
-/**
- * Updates the example in the virtual file cache
- *
- * @param {string} id
- * @param {string} code
- */
-function updateExample(id, code) {
-	const tree = ast.parse(code)
-
-	iterateMdsvexSrcNodes(tree, (srcNode, valueNode, i) => {
-		const exampleId = `${EXAMPLE_MODULE_PREFIX}${i}.svelte`
-		const virtualId = path.join('/', path.relative(process.cwd(), id + exampleId))
-
-		// store example code
-		virtualFiles.set(virtualId, unescape(valueNode.value.raw))
-	})
-}
-
-/**
- * Iterates over each __mdsvexample_src node in generated svelte file
- *
- * @param {any} tree
- * @param {(srcNode: any, valueNode: any, index: number) => void} cb
- */
-function iterateMdsvexSrcNodes(tree, cb) {
-	const exampleSrcNodes = ast.find(tree, {
-		type: 'Property',
-		key: {
-			name: '__mdsvexample_src'
-		}
-	})
-
-	exampleSrcNodes.map((node, i) => {
-		const [valueNode] = ast.find(node, {
-			type: 'TemplateElement'
-		})
-
-		if (valueNode) {
-			cb(node, valueNode, i)
-		}
-	})
-}
